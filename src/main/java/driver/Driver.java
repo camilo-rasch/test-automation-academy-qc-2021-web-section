fc97f3f6f5e7aa07cc9d0103995aae61be6284db
camilo mogollon <camilo.mogollon@outlook.com>
2021-06-09 09:57:14 -0500

    [ADD] added basic structure fw

---
+++ src/main/java/driver/Driver.java
@@ -0,0 +1,61 @@
+package driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Driver class
+ * @author camilo.mogollon
+ */
+public class Driver {
+
+	private WebDriver driver;
+
+	/**
+	 * Constructor.
+	 * @param browser String
+	 */
+	public Driver(String browser) {
+		switch (browser) {
+			case "remoteFirefox":
+				try {
+					driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.firefox());
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+				}
+				break;
+			case "remoteChrome":
+				try {
+					driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.chrome());
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+				}
+				break;
+			case "firefox":
+				System.setProperty("webdriver.gecko.driver", "./src/main/resources/geckodriverMac");
+				driver = new FirefoxDriver();
+				break;
+			case "chrome":
+				System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriverMac83");
+				driver = new ChromeDriver();
+				break;
+			default:
+				break;
+		}
+	}
+
+	/**
+	 * Get the driver.
+	 * @return {@link WebDriver}
+	 */
+	public WebDriver getDriver() {
+		return this.driver;
+	}
+
+}
