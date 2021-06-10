800de81839247607923baa564b20fba39c5d26a1
Jessica Marcela Cardona Reinoso <jessica.cardona@globant.com>
2021-06-10 17:57:15 -0500

    Java class Page PaulHerokuApp - locators

---
+++ src/test/java/com/automation/web/tests/TestPagePaulHerokuApp.java
@@ -0,0 +1,19 @@
+package com.automation.web.tests;
+
+public class TestPagePaulHerokuApp {
+	
+	String testParagraph = "By.css(.container p:nth-child(3)) ";
+	String jukaKorpelaLink = "By.css(.container address:nth-child(6) a:nth-child(1))";
+	String bulletListNumberThree = "By.css(.container ul > li:nth-child(3))";
+	String bulletMenuListNumberOne = "By.css(.container menu li:nth-child(1))";
+	String bulletDirListNumberThree = "By.css(.container dir li:nth-child(3))";
+	String coolButton = "By.id(but)";
+	String resetButton = "By.id(f0)";
+	String radioButton1 = "By.css(#f3 ~ label)";
+	String selectThirdPosition = "By.css(#f11 option:nth-child(3))";
+	String tablesIcelandLandArea = "By.css(.container table:nth-child(18) tbody tr:nth-child(4) td:nth-child(3))";
+	String characterTestGreekLetter = "By.css(.container table:nth-child(21) tbody tr:nth-child(5) td:nth-child(3))";
+	String hyphenation = "By.css(.container p:nth-child(23))";
+	String javaScriptHyphenation = "By.css(.container h3:nth-child(26))";
+
+}
