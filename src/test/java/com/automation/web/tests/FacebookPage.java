79ceafa6fb40888dc655148e9bb3590081d7369d
Jessica Marcela Cardona Reinoso <jessica.cardona@globant.com>
2021-06-10 19:26:07 -0500

    jaca class Facebook page - locators

---
+++ src/test/java/com/automation/web/tests/FacebookPage.java
@@ -0,0 +1,13 @@
+package com.automation.web.tests;
+
+public class FacebookPage {
+	
+	String emailField = "By.id(email)";
+	String passwordField = "By.id(pass)";
+	String loginButton = "By.id(u_0_d_lm)";
+	String profileButtonTopRigth = "By.css(div.bp9cbjyn.j83agx80.datstx6m.taijpn5t.oi9244e8.d74ut37n.dt6l4hlj.aferqb4h.q5xnexhs a span span)";
+	String friendsButton = "By.css(div div div a:nth-child(4) div.bp9cbjyn.rq0escxv.j83agx80.pfnyh3mw.frgo5egb.l9j0dhe7.cb02d2ww.hv4rvrfc.dati1w0a > span)";
+	String facebookAccount = "By.css(div.du4w35lb.l9j0dhe7.byvelhso.rl25f0pe.j83agx80.bp9cbjyn > span div div.oajrlxb2.tdjehn4e.qu0x051f.esr5mh6w.e9989ue4.r7d6kgcz.rq0escxv.nhd2j8a9.j83agx80.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.i1ao9s8h.esuyzwwr.f1sip0of.lzcic4wl.l9j0dhe7.abiwlrkh.p8dawk7l.bp9cbjyn.s45kfl79.emlxlaya.bkmhp75w.spb7xbtv.rt8b4zig.n8ej3o3l.agehan2d.sk4xxmp2.taijpn5t.qypqp5cg.q676j6op div)";
+	String facebookLogOut = "By.css(div:nth-child(4) > div > div.ow4ym5g4.auili1gw.rq0escxv.j83agx80.buofh1pr.g5gj957u.i1fnvgqd.oygrvhab.cxmmr5t8.hcukyx3x.kvgmc6g5.nnctdnn4.hpfvmrgz.qt6c0cv9.jb3vyjys.l9j0dhe7.du4w35lb.bp9cbjyn.btwxx1t3.dflh9lhu.scb9dxdr > div.ow4ym5g4.auili1gw.rq0escxv.j83agx80.buofh1pr.g5gj957u.i1fnvgqd.oygrvhab.cxmmr5t8.hcukyx3x.kvgmc6g5.tgvbjcpo.hpfvmrgz.qt6c0cv9.rz4wbd8a.a8nywdso.jb3vyjys.du4w35lb.bp9cbjyn.btwxx1t3.l9j0dhe7 div div div div span)";
+
+}
