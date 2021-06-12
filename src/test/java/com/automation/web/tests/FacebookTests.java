314e48f5e3dc99d40251a53fcfed389c7a80985c
Jonathan Triana Sarmiento <jonathan.triana@globant.com>
2021-06-11 19:58:39 -0500

    Put the classes ChangePhoto and DeleteFriend in a class called FacebookTests.java

--- src/test/java/com/automation/web/tests/FacebookTests.java
+++ src/test/java/com/automation/web/tests/FacebookTests.java
@@ -3,58 +3,58 @@ package com.automation.web.tests;
 import org.apache.log4j.Logger;
 import org.testng.annotations.*;
 /**
- * Class for the Change my profile picture on facebook.
+ * Class for the Delete friend and change photo on facebook.
  * @author jonathan.triana
  */
-public class ChangePhoto {
-    public Logger log = Logger.getLogger(ChangePhoto.class);
+public class FacebookTests {
+    public Logger log = Logger.getLogger(FacebookTests.class);
+
     @BeforeSuite()
     public void initiateDriver() {
         log.info("Initiate the chrome driver");
     }
+
     @BeforeTest()
     public void openBrowser() {
         log.info("Open Browser");
     }
+
     @BeforeClass()
     public void openFacebook() {
         log.info("Open Facebook page with this url: https://www.facebook.com/");
-    }
-    @BeforeMethod()
-    public void inputEmail() {
-        log.info("Input right email");
-    }
-    @BeforeMethod()
-    public void inputPassword() {
-        log.info("Input right password");
+        log.info("If you do not have facebook, create a new account with friends and photo");
     }
     @BeforeMethod()
     public void logIn() {
+        log.info("Go to facebook login");
+        log.info("Input right user");
+        log.info("Input right password");
         log.info("Click login button");
     }
-    @Test(description = "Open my profile") public void testCaseOpenMyProfile() {
+    @Test(description = "Open my profile") public void testCaseDeleteFriend() {
         log.info("Open my profile");
+        log.info("Open the friends section");
+        log.info("Select a friend");
+        log.info("Delete friend from my friends");
     }
-    @Test(description = "Update profile picture") public void testCaseUpdateProfilePicture() {
-        log.info("In the photo choose the update profile picture option");
-    }
-    @Test(description = "Select a photo") public void testCaseSelectPhoto() {
+    @Test(description = "Open my profile") public void testCaseChangeMyProfile() {
+        log.info("Open my profile");
+        log.info("In the photo choose the 'update profile picture' option");
         log.info("Select a photo");
-    }
-    @Test(description = "Save the new picture") public void testCaseSavePicture() {
         log.info("Save the new picture");
     }
-
     @AfterMethod()
     public void logOut() {
         log.info("Log out");
     }
+
     @AfterTest()
-    public void afterTest() {
+    public void closeBrowser() {
         log.info("Close Browser");
     }
+
     @AfterSuite()
-    public void afterSuite() {
+    public void endDriver() {
         log.info("End the chrome driver");
     }
 }
