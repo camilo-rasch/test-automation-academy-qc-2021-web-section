6fbc3f1e3274bdd1d8ee8d6e983367c34bac4932
Jhon Sebastian Martinez Soto <jhon.martinez@globant.com>
2021-06-13 17:47:43 -0500

    Add the last selectors on the files

--- src/test/java/com/web/automation/test/FacebookWebsite.java
+++ src/test/java/com/web/automation/test/FacebookWebsite.java
@@ -10,7 +10,16 @@ public class FacebookWebsite {
     String buttonLogin = "By.id(u_0_h_TN)";
 
     //Logout
+    String arrowDropdownMenuNavbar = "By.css(div[aria-label] > span > div[class] div[aria-label])";
+    String optionLogoutDropdown = "By.css(div.knvmm38d div[aria-label='Cuenta'] div[data-nocookies] div[role='button'])";
 
+    //MyProfile
+    //The user should be logued
+    String myProfileButtonNavbar = "By.css(a[href='/me/'])";
+
+    //MyFriends
+    //The user must be located on the profile
+    String myFriendsOption = "By.css(div[role='tablist']:nth-child(1) a:nth-child(4))";
 
 
 }
--- src/test/java/com/web/automation/test/HerokuTestWebsite.java
+++ src/test/java/com/web/automation/test/HerokuTestWebsite.java
@@ -8,4 +8,9 @@ public class HerokuTestWebsite {
     String coolButton = "By.id(#but)";
     String resetButton = "By.id(#f0)";
     String radioButton1 = "By.id(#f3)";
+    String thirdOptionDropdownMenu = "By.css(#f11 option:nth-child(3))";
+    String secondValueIceland = "By.css(table[summary] tbody tr:nth-child(4) td:nth-child(3)";
+    String secondValueCapitalOmega = "By.css(table tbody:nth-child(1) tr:nth-child(5) td:nth-child(3))";
+    String textHyphenation = "By.css(p:nth-child(23))";
+    String titleJavaScriptHyphenation = "By.css(p:nth-child(23))";
 }
