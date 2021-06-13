12d3825a1399042a9169085a726c3f98e75c8791
Jhon Sebastian Martinez Soto <jhon.martinez@globant.com>
2021-06-13 02:59:59 -0500

    add strings for elements on the heroku website file

---
+++ src/test/java/com/web/automation/test/FacebookWebsite.java
@@ -0,0 +1,16 @@
+package com.web.automation.test;
+
+import org.openqa.selenium.By;
+
+public class FacebookWebsite {
+
+    //Login
+    String inputEmail = "By.id(email)";
+    String inputPassword = "By.id(email)";
+    String buttonLogin = "By.id(u_0_h_TN)";
+
+    //Logout
+
+
+
+}
---
+++ src/test/java/com/web/automation/test/HerokuTestWebsite.java
@@ -0,0 +1,11 @@
+package com.web.automation.test;
+
+public class HerokuTestWebsite {
+    String linkJukkaKorpela = "By.css(address:nth-child(6) a[href='../personal.html'])";
+    String thirdElementList = "By.css(ul li:nth-child(3))";
+    String firstElementMenuList = "By.css(menu li:nth-child(1))";
+    String thirdElementDirList = "By.css(dir li:nth-child(3))";
+    String coolButton = "By.id(#but)";
+    String resetButton = "By.id(#f0)";
+    String radioButton1 = "By.id(#f3)";
+}
