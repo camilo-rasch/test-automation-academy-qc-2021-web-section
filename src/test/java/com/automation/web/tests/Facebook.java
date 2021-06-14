199445615cccd1fa52d528bb118187d31c398526
Silvana Perez Rojas <silvana.perez@globant.com>
2021-06-14 16:58:35 -0500

    Facebook and Herokuapp classes, containing web elements By css selector

---
+++ src/test/java/com/automation/web/tests/Facebook.java
@@ -0,0 +1,24 @@
+package com.automation.web.tests;
+
+public class Facebook {
+
+    String enterEmail = "By.css #email";
+
+    String enterPassword = "By.css #passContainer'";
+
+    String logInButton = "By.css #u_0_m_Wx ";
+
+    String myProfile = "By.css .buofh1pr div a[href='https://www.facebook.com/david.goez']:first-child ";
+
+    String friendsButton = "By.css .soycq5t1.l9j0dhe7 div a[href='https://www.facebook.com/david.goez/friends'] ";
+
+    String friendsList = "By.css .j83agx80.btwxx1t3.lhclo0ds.i1fnvgqd:nth-child(3)";
+
+    String SelectFriend = "By.css .dati1w0a.ihqw7lf3.hv4rvrfc.discj3wi:nth-child(2) ";
+
+    String SendMessage = "By.css .k4urcfbm div[aria-label='Mensaje'] ";
+
+    String menuButton = "By.css div[aria-label='Cuenta'] i[class='hu5pjgll lzf7d6o1'] ";
+
+    String signOutButton = "By.css div[data-nocookies='true'] div[role='button'] ";
+}
---
+++ src/test/java/com/automation/web/tests/Herokuapp.java
@@ -0,0 +1,31 @@
+package com.automation.web.tests;
+
+public class Herokuapp {
+
+    String testParagraph = "By.css .container p:nth-child(3) ";
+
+    String autorNameLink = "By.css a[href='../personal.html'][target='_blank'] ";
+
+    String listsElement = "By.css ul li:nth-child(3) ";
+
+    String menuListElement = "By.css(menu li:nth-child(1) ";
+
+    String dirListElement = "By.css dir li:nth-child(3) ";
+
+    String aCoolButton = "By.css #but ";
+
+    String resetButton = "By.css #f0 ";
+
+    String readioButton1 = "By.css #f3 ";
+
+    String selectlistboxElement = "By.css #f11 option:nth-child(3) ";
+
+    String elementInTable = "By.css .container div:nth-child(1) > table:nth-child(18) tr:nth-child(4) > td:nth-child(3) ";
+
+    String elementCharTestTable = "By.css .container div:nth-child(1) > table:nth-child(21) > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(3) ";
+
+    String hyphenationParagraf = "By.css .container p:nth-child(23) ";
+
+    String JsDrivenHyphenationTitle = "By.css .container h3:nth-child(26) ";
+
+}
