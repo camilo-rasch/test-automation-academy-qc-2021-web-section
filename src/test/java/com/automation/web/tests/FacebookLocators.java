30de95fdea94167a4dd13fd96e77ebd2b30a0d15
Jonathan Triana Sarmiento <jonathan.triana@globant.com>
2021-06-14 16:43:39 -0500

    Add FecebookLocators.java file

---
+++ src/test/java/com/automation/web/tests/FacebookLocators.java
@@ -0,0 +1,19 @@
+package com.automation.web.tests;
+/**
+ * Class for identify the locator in Facebook page.
+ * @author jonathan.triana
+ */
+public class FacebookLocators {
+    String email = "By.css (#email)";
+
+    String password = "By.css (#pass)";
+
+    String logInButton = "By.css (#u_0_h_1w)";
+
+    String myProfileOptionTopRight = "By.css (div.fop5sh7t.fv0vnmcu.j83agx80)";
+
+    String myFriendsOption = "By.css (div.soycq5t1.l9j0dhe7 div:nth-child(1) a:nth-child(4))";
+
+    String logOut = "By.css (div.b20td4e0.muag1w35 div:nth-child(4))";
+
+}
--- src/test/java/com/automation/web/tests/Herokuapp.java
+++ src/test/java/com/automation/web/tests/Herokuapp.java
@@ -1,36 +1,34 @@
 package com.automation.web.tests;
 
-import org.apache.log4j.Logger;
-import org.testng.annotations.*;
 /**
- * Class for identify the locator in Herokuapp pge.
+ * Class for identify the locator in Herokuapp page.
  * @author jonathan.triana
  */
 public class Herokuapp {
 
-    String paragraph = "By.css .container p:nth-child(3)";
+    String paragraph = "By.css (.container p:nth-child(3))";
 
-    String jukkaLink = "By.css .container address:nth-child(6) > a:nth-child(1)";
+    String jukkaLink = "By.css (address:nth-child(6) > a:nth-child(1))";
 
-    String listThree = "By.css .container ul:nth-child(8) > li:nth-child(3)";
+    String listThree = "By.css (ul:nth-child(8) > li:nth-child(3))";
 
-    String menuListOne = "By.css .container menu:nth-child(10) > li:nth-child(1)";
+    String menuListOne = "By.css (menu > li:nth-child(1))";
 
-    String dirListThree = "By.css .container dir:nth-child(12) > li:nth-child(3)";
+    String dirListThree = "By.css (dir> li:nth-child(3))";
 
-    String coolButton = "By.css #but";
+    String coolButton = "By.css (#but)";
 
-    String resetButton = "By.css #f0";
+    String resetButton = "By.css (#f0)";
 
-    String legendRadio = "By.css #f3";
+    String legendRadio = "By.css (#f3)";
 
-    String listBoxThree = "By.css #f11 option:nth-child(3)";
+    String listBoxThree = "By.css (#f11 option:nth-child(3))";
 
-    String tablesIcelandLandArea = "By.css .container tbody:nth-child(2) tr:nth-child(4) > td:nth-child(3)";
+    String tablesIcelandLandArea = "By.css (tbody:nth-child(2) tr:nth-child(4) > td:nth-child(3))";
 
-    String hyphenationParagraph = "By.css .container p:nth-child(23)";
+    String hyphenationParagraph = "By.css (p:nth-child(23))";
 
-    String characterOmegaNotes = ".container tbody:nth-child(1) tr:nth-child(5) > td:nth-child(3)";
+    String characterOmegaNotes = "By.css (tbody:nth-child(1) tr:nth-child(5) > td:nth-child(3))";
 
-    String javaScriptDrivenHyphenation = ".container h3:nth-child(26)";
+    String javaScriptDrivenHyphenation = "By.css (h3:nth-child(26))";
 }
