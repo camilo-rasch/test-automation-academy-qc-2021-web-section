8cedb08574bb720d8af898dacbf33b5a4bb45513
camilo mogollon <camilo.mogollon@outlook.com>
2021-06-15 18:40:12 -0500

    [Add] basic structure

--- src/test/java/com/web/automation/pages/HomePage.java
+++ src/test/java/com/web/automation/pages/HomePage.java
@@ -13,18 +13,6 @@ import java.util.Optional;
  */
 public class HomePage extends BasePage {
 
-    @FindBy(className = "central-textlogo__image")
-    private WebElement title;
-
-    @FindBy(id = "searchInput")
-    private WebElement searchField;
-
-    @FindBy(className = "pure-button")
-    private WebElement searchButton;
-
-    @FindBy(css = ".link-box Strong")
-    private List<WebElement> languages;
-
     /**
      * Constructor.
      * @param driver WebDriver
@@ -34,16 +22,4 @@ public class HomePage extends BasePage {
         super(driver);
         driver.get(url);
     }
-
-    /**
-     * Get Text title
-     * @return String text title
-     */
-    public String getTitle() {
-        log.info("Get Text title");
-        waitElementVisibility(title);
-        return title.getText();
-    }
-
-
 }
