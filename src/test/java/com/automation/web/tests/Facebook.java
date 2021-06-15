093733a67e5fda43f6907c2e2e907efd465f8f21
yefortiz <jey.ortiz@gmail.com>
2021-06-14 19:09:37 -0500

    Se realiza ejercicio de Herokuapp y parte de la de facebook, por motivos de salud no se continua por hoy

---
+++ src/test/java/com/automation/web/tests/Facebook.java
@@ -0,0 +1,11 @@
+package com.automation.web.tests;
+
+public class Facebook {
+
+    String elemento1 = "#email"; //Correo
+    String elemento2 = "#pass"; //Contraseña
+    String elemento3 = "#[data-testid*='royal_login_button']"; //Botòn de aceptar
+
+
+
+}
--- src/test/java/com/automation/web/tests/herokuapp.java
+++ src/test/java/com/automation/web/tests/herokuapp.java
@@ -2,6 +2,16 @@ package com.automation.web.tests;
 
 public class herokuapp {
 
-    String elemento1 = "By.css asda";
-    String elemento2 = "By.css #miPrimerElementoConId a.miPrimeraClase";
+    String elemento1 = ".container p:nth-child(3)";
+    String elemento2 = "body > div > address:nth-child(6) > a:nth-child(1)";
+    String elemento3 = "body > div > ul > li:nth-child(3)";
+    String elemento4 = "body > div > menu > li:nth-child(1)";
+    String elemento5 = "#but";
+    String elemento6 = "#f0";
+    String elemento7 = "#f3";
+    String elemento8 = "#f11 option:nth-child(2)";
+    String elemento9 = "table > tbody > tr:nth-child(4) > td:nth-child(3)";
+    String elemento10 = "table:nth-child(21) > tbody > tr:nth-child(5) > td:nth-child(3)";
+    String elemento11 = "body > div > p:nth-child(23)";
+    String elemento12 = "body > div > h3:nth-child(26)";
 }
