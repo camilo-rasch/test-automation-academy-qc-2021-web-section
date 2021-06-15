68277234129eed4d7f4c545e2b5244d5386784c7
Lina Gallego <82072687+MarcelaTIC00@users.noreply.github.com>
2021-06-15 15:59:57 -0500

    Update Facebook.java

--- Facebook.java
+++ Facebook.java
@@ -1,7 +1,7 @@
 public class Facebook {
 //https://www.facebook.com/
 
-    //Login
+      //Login
 	String email ="By.id #email";
 	String pass ="By.id #pass";
 	String loing="By.id #u_0_h_Fj";
@@ -12,11 +12,13 @@ public class Facebook {
 	String logoutButton="By.css [data-visualcompletion='ignore-dynamic']:nth-of-type(4) [data-visualcompletion='ignore']";
 
 	//My Profile
-	String myProfile="By.css a[href="/me/"]";
+	String myProfile="By.css a[href='/me/']";
 	
 
 	//My Friends
-	String myFriends ="By.xpath "[data-pagelet='ProfileTabs'] .kr520xx4 .j1lvzwm4:nth-child(7)";
+	String myFriends ="By.xpath [data-pagelet='ProfileTabs'] .kr520xx4 .j1lvzwm4:nth-child(7)";
+
+
 
 
 
