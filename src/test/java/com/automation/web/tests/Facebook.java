b15776901c42d1d8a2a2be884d52255a8727cd50
Fabio Alberto Alarcon Rodriguez <fabio.alarcon@globant.com>
2021-06-15 15:52:57 -0500

    Adding Taller2. Finding elements

---
+++ src/test/java/com/automation/web/tests/Facebook.java
@@ -0,0 +1,18 @@
+package com.automation.web.tests;
+
+public class Facebook {
+    // LOGIN
+    String elemento1 = "By.css #email";
+    String elemento2 = "By.css #pass";
+    String elemento3 = "By.css #u_0_d_Oc";
+
+    //LOGOUT
+    String elemento4 = "By.css div.rq0escxv.l9j0dhe7.du4w35lb div.ehxjyohh.kr520xx4.poy2od1o.b3onmgus.hv4rvrfc.n7fi1qx3:nth-child(4) div.__fb-light-mode:nth-child(2) div.j34wkznp.qp9yad78.pmk7jnqg.kr520xx4:nth-child(1) div.iqfcb0g7.tojvnm2t.a6sixzi8.k5wvi7nf.q3lfd5jv.pk4s997a.bipmatt0.cebpdrjk.qowsmv63.owwhemhu.dp1hu0rb.dhp61c6y.l9j0dhe7.iyyx5f41.a8s20v7p:nth-child(1) div.knvmm38d div.l9j0dhe7 div.rq0escxv.jgsskzai.cwj9ozl2.nwpbqux9.io0zqebd.m5lcvass.fbipl8qg.nwvqtn77.ni8dbmo4.stjgntxs div.o8kakjsu.rpm2j7zs.k7i0oixp.gvuykj2m.j83agx80.cbu4d94t.d76ob5m9.eg9m0zos.l9j0dhe7.du4w35lb.qan41l3s.c3g1iek1.t1wsaese.h77mwsce.o36gj0jk div.j83agx80.cbu4d94t.buofh1pr.l9j0dhe7 div.idiwt2bm.lzcic4wl.ni8dbmo4.stjgntxs.l9j0dhe7.dbpd2lw6 div.rq0escxv.pmk7jnqg.du4w35lb.pedkr2u6.oqq733wu.ms05siws.pnx7fd3z.b7h9ocf4.j9ispegn.kr520xx4.k4urcfbm:nth-child(1) div.aov4n071 div.a8nywdso.sj5x9vvc.rz4wbd8a.ecm0bbzt:nth-child(5) div.b20td4e0.muag1w35 div:nth-child(4) div.oajrlxb2.gs1a9yip.g5ia77u1.mtkw9kbi.tlpljxtp.qensuy8j.ppp5ayq2.goun2846.ccm00jje.s44p3ltw.mk2mc5f4.rt8b4zig.n8ej3o3l.agehan2d.sk4xxmp2.rq0escxv.nhd2j8a9.a8c37x1j.mg4g778l.btwxx1t3.pfnyh3mw.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.tgvbjcpo.hpfvmrgz.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.l9j0dhe7.i1ao9s8h.esuyzwwr.f1sip0of.du4w35lb.lzcic4wl.abiwlrkh.p8dawk7l.ue3kfks5.pw54ja7n.uo3d90p7.l82x9zwi > div.ow4ym5g4.auili1gw.rq0escxv.j83agx80.buofh1pr.g5gj957u.i1fnvgqd.oygrvhab.cxmmr5t8.hcukyx3x.kvgmc6g5.nnctdnn4.hpfvmrgz.qt6c0cv9.jb3vyjys.l9j0dhe7.du4w35lb.bp9cbjyn.btwxx1t3.dflh9lhu.scb9dxdr";
+
+    //MI PERFIL
+    String elemento5 = "By.css div.s45kfl79.emlxlaya.bkmhp75w.spb7xbtv.oaz4zybt.pmk7jnqg.j9ispegn.kr520xx4:nth-child(2)";
+
+    //MIS AMIGOS
+    String elemento6 = "By.css div.i09qtzwb.rq0escxv.n7fi1qx3.pmk7jnqg.j9ispegn.kr520xx4 a.oajrlxb2.g5ia77u1.qu0x051f.esr5mh6w.e9989ue4.r7d6kgcz.rq0escxv.nhd2j8a9.pq6dq46d.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.i1ao9s8h.esuyzwwr.f1sip0of.lzcic4wl.l9j0dhe7.abiwlrkh.p8dawk7l.dwo3fsh8.ow4ym5g4.auili1gw.mf7ej076.gmql0nx0.tkr6xdv7.bzsjyuwj.cb02d2ww.j1lvzwm4:nth-child(4) > div.bp9cbjyn.rq0escxv.j83agx80.pfnyh3mw.frgo5egb.l9j0dhe7.cb02d2ww.hv4rvrfc.dati1w0a";
+
+}
--- src/test/java/com/automation/web/tests/herokuapp.java
+++ src/test/java/com/automation/web/tests/herokuapp.java
@@ -2,6 +2,17 @@ package com.automation.web.tests;
 
 public class herokuapp {
 
-    String elemento1 = "By.css asda";
-    String elemento2 = "By.css #miPrimerElementoConId a.miPrimeraClase";
+    String elemento1 = "By.css address:nth-child(6) > a:nth-child(1)";
+    String elemento2 = "By.css ul:nth-child(8) > li:nth-child(3)";
+    String elemento3 = "By.css menu:nth-child(10) > li:nth-child(1)";
+    String elemento4 = "By.css dir:nth-child(12) > li:nth-child(3)";
+    String elemento5 = "By.css #but";
+    String elemento6 = "By.css #f0";
+    String elemento7 = "By.css #f3";
+    String elemento8 = "By.css div:nth-child(11) select:nth-child(3) > option:nth-child(3)";
+    String elemento9 = "By.css tbody:nth-child(2) tr:nth-child(4) > td:nth-child(3)";
+    String elemento10 = "By.css tbody:nth-child(1) tr:nth-child(5) > td:nth-child(3)";
+    String elemento11 = "By.css div.container:nth-child(1) > p:nth-child(23)";
+    String elemento12 = "By.css div.container:nth-child(1) > h3:nth-child(26)";
+
 }
