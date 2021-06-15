9df703f0dcf13a28189051156ad348cfc51ec322
Marcela00 <libelula1701@gmail.com>
2021-06-14 19:58:47 -0500

    Added the selectors taller 2

---
+++ Facebook.java
@@ -0,0 +1,23 @@
+public class Facebook {
+//https://www.facebook.com/
+
+    //Login
+	String email ="By.id #email";
+	String pass ="By.id #pass";
+	String loing="By.id #u_0_h_Fj";
+
+
+	//Logout
+	String logoutList="By.css [role]>.iyyx5f41:nth-of-type(1) [role]";
+	String logoutButton="By.css [data-visualcompletion='ignore-dynamic']:nth-of-type(4) [data-visualcompletion='ignore']";
+
+	//My Profile
+	String myProfile="By.css a[href="/me/"]";
+	
+
+	//My Friends
+	String myFriends ="By.xpath "[data-pagelet='ProfileTabs'] .kr520xx4 .j1lvzwm4:nth-child(7)";
+
+
+
+}
---
+++ Testing_Herokuapp.java
@@ -0,0 +1,35 @@
+
+
+
+public class Testing_Herokuapp {
+//http://test-page-paul.herokuapp.com/
+
+String testParagraph = "By.css p:nth-child(3)"; 
+
+String jukkaLink= "By.css address>a[target=_blank]";
+
+String 3List= "By.css ul li:nth-of-type(3)"; 
+
+String 1menu= "By.css menu li:nth-of-type(1)"; 
+
+String 3DirList = "By.css dir li:nth-of-type(3)"; 
+
+String coolButton = "By.id #but";
+
+String resetButton= "By.id #f0"; 
+
+String radioButton1 = "By.id #f3"; 
+
+String 3Listbox = "By.css #f11>option:nth-of-type(3)"; 
+
+String IcelandTable = "By.css table[summary*=Nordic]>tbody>tr:nth-of-type(4) td:nth-child(3)"; 
+
+String capitalOmegaTable = "By.css table>tbody>tr:nth-child(5)>td:nth-of-type(3)"; 
+
+String hyphensPara = "By.css h2+p:nth-child(23)"; 
+
+String hyphensTitle= "By.class .hyphens"; 
+
+
+
+}
