b981d5139eebb24d5f290a4aadca6cd86b1196ce
Juan Daniel Castano Ortega <juandaniel.castano@globant.com>
2021-06-15 15:53:13 -0500

    Exercise 2 delivered, css Selector

---
+++ src/test/java/com/automation/web/tests/Herokuapp.java
@@ -0,0 +1,42 @@
+package com.automation.web.tests;
+
+public class Herokuapp {
+
+    //First element
+    String element1 = "By.css address:nth-child(6) > a:nth-child(1)";
+
+    //Second Element
+    String element2 = "By.css ul:nth-child(8) > li:nth-child(3)";
+
+    //Third Element
+    String element3 = "By.css menu:nth-child(10) > li:nth-child(1)";
+
+    //Fourth Element
+    String element4 = "By.css dir:nth-child(12) > li:nth-child(3)";
+
+    //Fifth Element
+    String element5 = "By.css #but";
+
+    //Sixth Element
+    String element6 = "By.css #fo";
+
+    //Seventh Element
+    String element7 = "By.css #f3";
+
+    //Eighth Element
+    String element8 = "By.css div:nth-child(11) select:nth-child(3) > option:nth-child(3)";
+
+    //Ninth Element
+    String element9 = "By.css tbody:nth-child(2) tr:nth-child(4) > td:nth-child(3)";
+
+    //Tenth Element
+    String element10 = "By.css tbody:nth-child(1) tr:nth-child(5) > td:nth-child(3)";
+
+    //Eleventh Element
+    String element11 = "By.css div.container:nth-child(1) > p:nth-child(23)";
+
+    //Twelfth Element
+    String element12 = "By.css div.container:nth-child(1) > h3:nth-child(26)";
+
+
+}
--- src/test/java/com/automation/web/tests/herokuapp.java
+++
@@ -1,7 +0,0 @@
-package com.automation.web.tests;
-
-public class herokuapp {
-
-    String elemento1 = "By.css asda";
-    String elemento2 = "By.css #miPrimerElementoConId a.miPrimeraClase";
-}
