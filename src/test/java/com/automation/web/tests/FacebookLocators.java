162b45daaae8b36d070f057d3d08014a41213697
Harvey Daniel Yepes Mesa <harvey.yepes@globant.com>
2021-06-14 19:41:34 -0500

    Taller_2_Locators

---
+++ src/test/java/com/automation/web/tests/FacebookLocators.java
@@ -0,0 +1,37 @@
+package com.automation.web.tests;
+
+
+public class FacebookLocators {
+
+    /**
+     * Class for the in user test.
+     * @author Harvey.Yepes
+     */
+
+    //locators en CSS del login de Facebook
+    //locators: #email, #pass, ._featuredLogin__formContainer div:nth-child(6)>button
+    //Log In page:
+    String email = "By.id #email";
+    String password = "By.id #pass";
+    String loginButton = "By.css ._featuredLogin__formContainer div:nth-child(6)>button";
+
+
+    //Log out:
+    //Primero dar Click en “Cuenta” y luego en “Cerrar Sesión”
+    //locators: [role="banner"]  > div:nth-child(4) > div > span > div > div:first-child,
+    // div:nth-child(4) div:nth-child(2) >div div:last-of-type> div:nth-child(5) > div > div:nth-child(4) > div > div:nth-child(1)
+
+    String clickOnAccount = "By.css [role=\"banner\"]  > div:nth-child(4) > div > span > div > div:first-child";
+    String clickOnLogOut = "By.css div:nth-child(4) div:nth-child(2) >div div:last-of-type> div:nth-child(5) > div > div:nth-child(4) > div > div:nth-child(1)";
+
+    //Mi perfil
+    //Dar click en el nombre
+    String profile = "By.css [role=\"banner\"] > div:nth-child(4)>div:first-child>div:last-child>a";
+
+    //Mis amigos
+    //Primero dar click en “Mi perfil” y luego click en “Amigos”
+    //locators: [role="banner"] > div:nth-child(4)>div:first-child>div:last-child>a, [role="tablist"] > div > div >a:nth-child(4)
+
+    String friends = "By.css [role=\"tablist\"] > div > div >a:nth-child(4)";
+
+}
---
+++ src/test/java/com/automation/web/tests/HerokuappLocators.java
@@ -0,0 +1,24 @@
+package com.automation.web.tests;
+
+public class HerokuappLocators {
+
+    /**
+     * Class for the in user test.
+     * @author Harvey.Yepes
+     */
+
+    //locators en CSS de los pantallazos de la página http://test-page-paul.herokuapp.com/
+    String photo1 = "By.css .container address:nth-child(6) > a:nth-child(1)";
+    String photo2 = "By.css .container ul>li:nth-child(3)";
+    String photo3 = "By.css .container menu>li:nth-child(1)";
+    String photo4 = "By.css .container dir>li:nth-child(3)";
+    String photo5 = "By.id #but";
+    String photo6 = "By.id #f0";
+    String photo7 = "By.id #f3";
+    String photo8 = "By.css #f11 > option:nth-child(3)";
+    String photo9 = "By.css .container table:nth-child(18) > tbody > tr:nth-child(4) > td:nth-child(3)";
+    String photo10 = "By.css .container table:nth-child(21) > tbody > tr:nth-child(5) > td:nth-child(3)";
+    String photo11 = "By.css .container p:nth-child(23)";
+    String photo12 = "By.css .container h3:nth-child(26)";
+
+}
