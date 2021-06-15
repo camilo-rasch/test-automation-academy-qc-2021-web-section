5ee4a98c890ee19144b18c8ff09f1bbbc30f79d6
Hernan Enrique Barroso Garavito <hernan.barroso@globant.com>
2021-06-14 23:22:56 -0500

    herokuapp locators and facebook locators

---
+++ src/test/java/com/automation/web/tests/facebookPage.java
@@ -0,0 +1,19 @@
+package com.automation.web.tests;
+
+public class facebookPage {
+
+    String email = "By.css(#email)";
+
+    String password = "By.css(#passContainer)";
+
+    String loginButton = "By.css(#u_0_h_GO)";
+
+    String rightProfileButton = "By.css(.bp9cbjyn a[href='/me/'])";
+
+    String friendsButton = "By.css(.soycq5t1 div a[href='https://www.facebook.com/hebarroso/friends'])";
+
+    String accountButton = "By.css([role='navigation'] > span div div:nth-child(1))";
+
+    String logoutOption = "By.css (.ecm0bbzt div:nth-child(4))";
+
+}
---
+++ src/test/java/com/automation/web/tests/testPagePaulHerokuApp.java
@@ -0,0 +1,17 @@
+package com.automation.web.tests;
+
+public class testPagePaulHerokuApp {
+
+    String paragraph = "By.css(.container p:nth-child(3))";
+    String JukkaKorpelaLink = "By.css(.container address:nth-child(6) a:nth-child(1))";
+    String bulletThree = "By.css(.container menu:nth-child(10) li:nth-child(3))";
+    String bulletOne = "By.css(.container menu:nth-child(10) li:nth-child(1))";
+    String aCoolButtonButton = "By.css(#but)";
+    String resetButton = "By.css(#f0)";
+    String radioButton1 = "By.css(#f3)";
+    String listboxThree = "By.css(.container div:nth-child(11) select:nth-child(3) > option:nth-child(3))";
+    String landAreaValue = "By.css(.container tr:nth-child(4) td:nth-child(3))";
+    String notesAGreekLetter = "By.css(.container tbody:nth-child(1) tr:nth-child(5) td:nth-child(3))";
+    String hyphenationDescrition = "By.css(.container p:nth-child(23))";
+    String javaScriptDrivenHyphenation = "By.css(.container h3:nth-child(26))";
+}
