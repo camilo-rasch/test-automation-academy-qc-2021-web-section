a3db87205c1abc4b013459abe0fbe677762ef5d2
camilo mogollon <camilo.mogollon@outlook.com>
2021-06-15 12:43:44 -0500

    [Add] twitter main page

---
+++ src/test/java/com/web/automation/pages/TwitterNewsFeed.java
@@ -0,0 +1,15 @@
+package com.web.automation.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class TwitterNewsFeed extends BasePage{
+
+    /**
+     * Constructor.
+     *
+     * @param pDriver WebDriver
+     */
+    public TwitterNewsFeed(WebDriver pDriver) {
+        super(pDriver);
+    }
+}
