95642443ccabe2f0f8f952a7be2b393515a6db0c
alejandro.giraldo <alejandro.giraldo@globant.com>
2021-06-15 15:15:20 -0500

    Locators para herokuapp y Facebook

---
+++ src/test/java/com/automation/web/tests/Facebook.java
@@ -0,0 +1,20 @@
+package com.automation.web.tests;
+
+public class Facebook {
+
+    //para Login
+    String elemento1 = "By.id email";
+    String elemento2 = "By.id pass";
+    String elemento3 = "By.id u_0_h_Pp";
+
+    //Para cierre de sesión
+    String elemento4 = "By.css div[role='button'] > img";
+    String elemento5 = "By.xpath //span[contains(text(), 'Cerrar sesión')]";
+
+    //Para Mi perfil
+    String elemento6 = "By.css[data-pagelet] .buofh1pr:nth-of-type(1) > ul:nth-of-type(1) .stjgntxs";
+
+    //Para mis Amigos
+    String elemento7 = "By.css li:nth-of-type(2) > .a6sixzi8.a8s20v7p.abs2jz4q.bipmatt0.cebpdrjk.dhp61c6y.dp1hu0rb.iyyx5f41.k5wvi7nf.owwhemhu.pk4s997a.q3lfd5jv.qowsmv63.t1p8iaqh.tojvnm2t  a[role='link']  .a8c37x1j.b7h9ocf4.em6zcovv.hwsy1cff.ms05siws";
+    
+}
--- src/test/java/com/automation/web/tests/herokuapp.java
+++ src/test/java/com/automation/web/tests/herokuapp.java
@@ -2,6 +2,16 @@ package com.automation.web.tests;
 
 public class herokuapp {
 
-    String elemento1 = "By.css asda";
-    String elemento2 = "By.css #miPrimerElementoConId a.miPrimeraClase";
+    String elemento1 = "By.css div.container>:nth-child(3)";
+    String elemento2 = "By.xpath (//a[@href='../personal.html'])[1]";
+    String elemento3 = "By.xpath (//li[contains(text(),'Three')])[1]";
+    String elemento4 = "By.id but";
+    String elemento5 = "By.id f0";
+    String elemento6 = "By.id f3";
+    String elemento7 = "By.xpath //select[@id='f10']/option[contains(text(), 'two')]";
+    String elemento8 = "By.xpath //td[contains(text(), '100,250')]";
+    String elemento9 = "By.xpath //td[contains(text(), 'A Greek letter')]";
+    String elemento10 = "By.css p.limited.hyphens";
+    String elemento11 = "By.css p.limited.hyphenate";
+
 }
