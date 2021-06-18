6186b92693dcf48789dc0a722d8cb6944ec961fb
Jhon Sebastian Martinez Soto <jhon.martinez@globant.com>
2021-06-18 01:05:42 -0500

    create the test, locators and methods for the task

--- src/test/java/com/web/automation/pages/HomePage.java
+++ src/test/java/com/web/automation/pages/HomePage.java
@@ -1,11 +1,14 @@
 package com.web.automation.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Class for interact with the home page.
@@ -13,13 +16,30 @@ import java.util.Optional;
  */
 public class HomePage extends BasePage {
 
+    @FindBy (css = "p[class=\"selected-value\"]")
+    private WebElement optionSelected;
+
+    Select dropDown;
+
     /**
      * Constructor.
      * @param driver WebDriver
      * @param url String
      */
+
     public HomePage(WebDriver driver, String url) {
         super(driver);
         driver.get(url);
+        this.dropDown = new Select(getDriver().findElement(By.id("select-demo")));
+    }
+
+    public void selectOptionDropdown() {
+        dropDown.selectByValue("Sunday");
+
+    }
+
+    public boolean isElementSelected() {
+        waitElementVisibility((this.optionSelected));
+        return optionSelected.isDisplayed();
     }
 }
---
+++ src/test/java/com/web/automation/test/TestSuite.java
@@ -0,0 +1,19 @@
+package com.web.automation.test;
+
+import com.web.automation.pages.HomePage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestSuite extends BaseTest {
+    HomePage homePage;
+
+    @Test(description = "This test select one option on the dropdown list")
+    public void selectOption(){
+        log.info("Open the url already given");
+        homePage = getHomePage();
+        log.info("Select a option on the dropdown");
+        homePage.selectOptionDropdown();
+        log.info("Validate if the option was select on the dropdown");
+        Assert.assertTrue(homePage.isElementSelected());
+    }
+}
