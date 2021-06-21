502c59e94f7430fde798758a33b1890274e71586
Hernan Enrique Barroso Garavito <hernan.barroso@globant.com>
2021-06-20 23:23:17 -0500

    Delivery TallerAlerts

--- src/test/java/com/web/automation/pages/HomePage.java
+++ src/test/java/com/web/automation/pages/HomePage.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 /**
  * Class for interact with the home page.
- * @author camilo.mogollon
+ * @author
  */
 public class HomePage extends BasePage {
 
@@ -22,38 +22,66 @@ public class HomePage extends BasePage {
     private WebElement submitButton;
 
 
+    /**
+     * Input text in the customerID field.
+     * @return
+     */
     public void insertDataCustomerID(String text){
      waitElementVisibility(customerID);
      this.customerID.sendKeys(text);
  }
 
+    /**
+     * Click on the submit button.
+     * @return
+     */
     public void clickOnSubmitButton(){
      waitElementVisibility(submitButton);
      this.submitButton.click();
     }
 
+    /**
+     * Capture and print alert message.
+     * @return
+     */
     public void CaptureAndPrintAlertMessage (){
         Alert alert = switchToAlert();
         String alertMessage = alert.getText();
         System.out.println(alertMessage);
     }
 
+    /**
+     * Click on the alert OK button.
+     * @return
+     */
     public void ClickAlertOkButton (){
         Alert alert = switchToAlert();
         alert.accept();
     }
 
+    /**
+     * Capture and print alert message.
+     * @return
+     */
     public void CaptureAndPrintSecondAlertMessage (){
         Alert alert = switchToAlert();
         String alertMessage2 = alert.getText();
         System.out.println(alertMessage2);
     }
 
+    /**
+     * Click on the alert OK button.
+     * @return
+     */
     public void ClickSecondAlertOkButton (){
         Alert alert = switchToAlert();
         alert.accept();
     }
 
+    /**
+     * Verify if submit button is displayed.
+     * @return boolean
+     */
     public boolean isSubmitDisplayed(){
         waitElementVisibility(submitButton);
         return submitButton.isDisplayed();
